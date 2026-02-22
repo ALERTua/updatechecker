50d16935f68fec174abbca62beacf8379f9bdf83
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2026-02-22 18:04:21 +0200

    temp folder fallback

--- src/updatechecker/constants.py
+++ src/updatechecker/constants.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 ROOT_FOLDER = Path(__file__).parent.parent.parent
-TEMP_FOLDER = Path(os.getenv('TEMP')) / 'updatechecker/'
+TEMP_FOLDER = Path(os.getenv('TEMP', '/tmp')) / 'updatechecker/'
 TEMP_FOLDER.mkdir(exist_ok=True)
 
 DEFAULT_CHUNK_SIZE = 10 * 1024 * 1024  # 10 MB in bytes
